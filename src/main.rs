3a4f62b1e55d553def27f6c591c89e2cbd9826ca
Brandon Holt <b.holt@apple.com>
2019-05-01 07:42:58 -0700

    working on adding line splitting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{stdin, BufRead};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -12,11 +13,28 @@ struct Config {
     #[structopt(short = "d", long = "delimiter", default_value = "\\s+")]
     delimiter: String,
 
+    #[structopt(short = "f", long = "field")]
+    field: Option<i32>,
+
     #[structopt(short = "v", long = "verbose")]
     verbose: bool,
+}
+
+impl Config {
+    fn handle_line(&self, l: String) -> String {
+        if let Some(f) = self.field {
+            let delim = self.delimiter;
+            return l.split(delim).nth(f);
+        }
+        return "".to_string();
+    }
 }
 
 fn main() {
     let config = Config::from_args();
     println!("{:#?}", config);
+
+    for line in stdin().lock().lines() {
+        println!(">>> {}", line.unwrap());
+    }
 }
