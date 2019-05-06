c21e64a361f2e420185b118a08fcc6c82298e973
Brandon Holt <b.holt@apple.com>
2019-05-05 17:47:26 -0700

    add config options, types for ParsedLine, OutputFormat, and start implementing with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,41 @@
-use std::io::BufRead;
-use std::io::Result;
+use regex::Regex;
+use std::collections::HashMap;
+use std::fmt;
 use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, Default, Eq, PartialEq)]
 #[structopt(
     name = "aline",
     author = "Brandon Holt",
     version = "0.1.0",
     about = "Replacement for awk/cut/sed. Never again struggle to remember which one does what you want or splits fields how you want, just switch to aline for all your line-parsing needs"
 )]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
 pub struct Config {
-    /// Delimiter to use for separating
+    #[structopt(parse(from_os_str))]
+    // Inputs passed as arguments
+    pub inputs: Vec<PathBuf>,
+
     #[structopt(short = "d", long = "delimiter", default_value = "\\s+")]
+    /// Delimiter to use for parsing fields, cannot be set along with --input.
     pub delimiter: String,
 
+    #[structopt(short = "i", long = "input")]
+    /// Input format (csv, json), cannot be set along with --delimiter.
+    pub input_format: Option<String>,
+
     #[structopt(short = "f", long = "field")]
+    // Field(s) to print, specified by number or name (depending on the input format).
     pub field: Option<usize>,
+
+    #[structopt(short = "k", long = "keys")]
+    // Field(s) to print, specified by number or name (depending on the input format).
+    pub fields: Vec<FieldSelector>,
 
     #[structopt(short = "v", long = "verbose")]
     pub verbose: bool,
-
-    #[structopt(parse(from_os_str))]
-    pub inputs: Vec<PathBuf>,
 
     #[structopt(short = "p", long = "print-filenames")]
     /// Print each filename prefixed with '#' before its results
@@ -30,47 +43,170 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn handle_line<'a>(&self, l: &'a str) -> Option<&'a str> {
-        if let Some(f) = self.field {
-            let mut parts = l.split(&self.delimiter);
-            return parts.nth(f);
+    fn default() -> Config {
+        Config {
+            inputs: vec![],
+            delimiter: "".to_string(),
+            input_format: None,
+            field: None,
+            fields: vec![],
+            verbose: false,
+            print_filename: false,
         }
-        return Some(l);
+    }
+
+    fn parser(&self) -> impl Fn(&str) -> ParsedLine {
+        let delim = Regex::new(&self.delimiter).unwrap();
+        // if Some(input) = &self.input_format {
+        return move |l: &str| {
+            let parts = delim.split(&l).map(String::from).collect::<Vec<String>>();
+            ParsedLine {
+                fields: parts,
+                keys: HashMap::new(),
+            }
+        };
     }
 }
 
-//pub fn process_reader<'a>(
-//    config: &'a Config,
-//    r: &'a mut impl BufRead,
-//) -> impl Iterator<Item = Option<String>> + 'a {
-//    return r.lines().map(move |rl| {
-//        let l = rl.unwrap();
-//        println!(">>> {}", &l);
-//        if let Some(f) = config.field {
-//            let mut parts = l.split(&config.delimiter);
-//            return parts.nth(f).map(String::from);
-//        }
-//        return Some(l);
-//    });
+/// Selects a field to output
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum FieldSelector {
+    Index(usize),
+    Key(String),
+}
+
+impl FromStr for FieldSelector {
+    type Err = std::io::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.parse::<usize>() {
+            Ok(i) => Ok(FieldSelector::Index(i)),
+            Err(_) => Ok(FieldSelector::Key(String::from(s))),
+        }
+    }
+}
+
+// ParsedLine contains the parsed fields of a line
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ParsedLine {
+    fields: Vec<String>,
+    keys: HashMap<String, String>,
+}
+
+// Output format to use when printing.
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum OutputFormat {
+    Space,
+    CSV,
+    JSON,
+    Custom(String),
+}
+
+impl ParsedLine {
+    fn new(fields: &[&str]) -> Self {
+        ParsedLine {
+            fields: fields.iter().map(|s| String::from(*s)).collect(),
+            keys: HashMap::new(),
+        }
+    }
+
+    fn field(&self, f: &FieldSelector) -> Option<String> {
+        match f {
+            FieldSelector::Index(i) => {
+                if i < &self.fields.len() {
+                    Some(self.fields[*i].clone())
+                } else {
+                    None
+                }
+            }
+            FieldSelector::Key(k) => self.keys.get(k).cloned(),
+        }
+    }
+
+    fn output(&self, format: OutputFormat, sel: Vec<FieldSelector>) -> String {
+        match format {
+            OutputFormat::Space => sel
+                .iter()
+                .map(|f| self.field(f))
+                .flatten()
+                .collect::<Vec<String>>()
+                .join(" "),
+            OutputFormat::CSV => sel
+                .iter()
+                .map(|f| self.field(f))
+                .flatten()
+                .map(|f| {
+                    if f.contains(",") {
+                        format!("\"{}\"", f)
+                    } else {
+                        f
+                    }
+                })
+                .collect::<Vec<String>>()
+                .join(","),
+            _ => unimplemented!(),
+        }
+    }
+}
+
+//fn run(config: &Config, lines: impl Iterator<Item = String>) -> impl Iterator<Item = String> {
+//    unimplemented!()
 //}
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
-    use std::io::{BufRead, BufReader};
+    use crate::{Config, FieldSelector, OutputFormat, ParsedLine};
+    use std::collections::HashMap;
+    use structopt::StructOpt;
+    use FieldSelector::*;
+    use OutputFormat::*;
+
+    /// Helper to create a Config from flags for testing
+    fn cfg(args: &[&str]) -> Config {
+        Config::from_iter_safe([&["aline"], args].concat()).expect("invalid config flags")
+    }
 
     #[test]
-    fn scratch() {
-        //        let f = File::open("Cargo.toml").unwrap();
-        //        let fbuf = BufReader::new(f);
-        //        let lines = fbuf.lines();
-        let xs = vec![1, 7, 8, 2, 6, 3, 4];
-        println!("{:?}", xs);
-        let ys = xs
-            .iter()
-            .flat_map(|x| if x > &5 { None } else { Some(x) })
-            .collect::<Vec<&i32>>();
-        println!("{:?}", ys);
-        assert!(false);
+    fn test_cfg_helper() {
+        assert_eq!(
+            Config {
+                delimiter: ",".to_string(),
+                ..Config::default()
+            },
+            cfg(&["-d,"]),
+        )
+    }
+
+    /// Helper to create parsed line for testing
+    fn pline(fields: &[&str]) -> ParsedLine {
+        ParsedLine {
+            fields: fields.iter().map(|s| String::from(*s)).collect(),
+            keys: HashMap::new(),
+        }
+    }
+
+    #[test]
+    fn parsed_line_output() {
+        for (ps, fmt, fs, out) in &[
+            (&["a", "b"], Space, vec![Index(0)], "a"),
+            (&["a", "b"], Space, vec![Index(1)], "b"),
+            (&["a", "b"], Space, vec![Index(0), Index(1)], "a b"),
+            (&["a", "b"], CSV, vec![Index(1)], "b"),
+            (&["a", "b"], CSV, vec![Index(0), Index(1)], "a,b"),
+            (&["a", "b,c"], CSV, vec![Index(0), Index(1)], "a,\"b,c\""),
+        ] {
+            assert_eq!(*out, pline(*ps).output(fmt.clone(), fs.to_vec()));
+        }
+    }
+
+    #[test]
+    fn parser() {
+        let p = cfg(&["-d,"]).parser();
+        assert_eq!(p("a,b"), pline(&["a", "b"]));
+    }
+
+    #[test]
+    fn test_fmt() {
+        assert_eq!("b a", format!("{1} {0}", "a", "b"));
     }
 }
