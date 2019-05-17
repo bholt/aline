f8a3d99c123f3591eef1dca563b8d0d9dfb3cacb
Brandon Holt <b.holt@apple.com>
2019-05-16 18:43:13 -0700

    trying again to work with iterators

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
-use aline::{output, Config};
+use aline::{output, Config, FieldSelector, Fields, InputFormat};
+use csv;
 use std::fs::File;
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader, Read};
 use structopt::StructOpt;
+
+struct CSVRecord {
+    r: csv::StringRecord,
+}
+
+impl Fields for CSVRecord {
+    fn field(&self, f: &FieldSelector) -> Option<String> {
+        match f {
+            FieldSelector::Index(i) => self.r.get(*i).map(|s| s.to_owned()),
+            FieldSelector::Key(_) => None,
+        }
+    }
+}
+
+struct CSVParser<'r, R: Read> {
+    r: csv::StringRecordsIter<'r, R>,
+}
+
+impl<'r, R: Read> CSVParser<'r, R> {
+    fn new(r: R + 'r) -> Self {
+        CSVParser {
+            r: csv::Reader::from_reader(r).records(),
+        }
+    }
+}
+
+impl<'r, R: Read> Iterator for CSVParser<'r, R> {
+    type Item = Box<dyn Fields>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.r.next() {
+            Some(r) => match r {
+                Ok(v) => Some(Box::new(CSVRecord { r: v })),
+                Err(e) => {
+                    eprintln!("record parsing error: {:?}", e);
+                    None
+                }
+            },
+            None => None,
+        }
+    }
+}
 
 fn main() {
     let config: Config = Config::from_args();
@@ -27,6 +70,13 @@ fn main() {
             println!("# {}", fname.to_str().unwrap());
         }
         let f = File::open(fname).unwrap();
+
+        if config.input_format == Some(InputFormat::CSV) {
+            for r in CSVParser::new(f) {
+                println!("{}", output(r.as_ref(), &config.output, &config.fields));
+            }
+        }
+
         let fbuf = BufReader::new(f);
 
         for line in fbuf.lines() {
