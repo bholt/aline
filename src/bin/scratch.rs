87b30031024cd9861ebdbe0725ed69c1f67bf734
Brandon Holt <holt.bg@gmail.com>
2024-01-20 13:31:49 -0800

    use modifiers in scratch

--- src/bin/scratch.rs
+++ src/bin/scratch.rs
@@ -1,9 +1,12 @@
-use comfy_table::{Table, presets};
+use comfy_table::{modifiers, presets, Table};
 
 fn main() {
     let mut table = Table::new();
+    // let mystyle = "││──╞═╪╡│─┼├┤┬┴┌┐└┘";
     table
         .load_preset(presets::UTF8_FULL)
+        .apply_modifier(modifiers::UTF8_ROUND_CORNERS)
+        .apply_modifier(modifiers::UTF8_SOLID_INNER_BORDERS)
         .set_header(vec!["Header1", "Header2", "Header3"])
         .add_row(vec![
             "This is a text",
