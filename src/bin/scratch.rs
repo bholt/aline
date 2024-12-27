3632948ad7df85c48f0d89dae4cdd592baca6b5f
Brandon Holt <holt.bg@gmail.com>
2024-12-27 15:24:56 -0800

    experiment with polars dataframe

--- src/bin/scratch.rs
+++ src/bin/scratch.rs
@@ -1,4 +1,11 @@
 use comfy_table::{modifiers, presets, Table};
+use polars::prelude::*;
+
+pub fn create_dataframe() -> PolarsResult<DataFrame> {
+    let s0 = Series::new("days".into(), &[0, 1, 2, 3, 4]);
+    let s1 = Series::new("temp".into(), &[22.1, 19.9, 7.0, 2.0, 3.0]);
+    DataFrame::new(vec![s0.into(), s1.into()]).map_err(|e| e.into())
+}
 
 fn main() {
     let mut table = Table::new();
@@ -6,18 +13,41 @@ fn main() {
     table
         .load_preset(presets::UTF8_FULL)
         .apply_modifier(modifiers::UTF8_ROUND_CORNERS)
-        .apply_modifier(modifiers::UTF8_SOLID_INNER_BORDERS)
-        .set_header(vec!["Header1", "Header2", "Header3"])
-        .add_row(vec![
-            "This is a text",
-            "This is another text",
-            "This is the third text",
-        ])
-        .add_row(vec![
-            "This is another text",
-            "Now\n add some\n multi line stuff",
-            "This is awesome",
-        ]);
+        .apply_modifier(modifiers::UTF8_SOLID_INNER_BORDERS);
+    //     .set_header(vec!["Header1", "Header2", "Header3"])
+    //     .add_row(vec![
+    //         "This is a text",
+    //         "This is another text",
+    //         "This is the third text",
+    //     ]);
+    //     .add_row(vec![
+    //         "This is another text",
+    //         "Now\n add some\n multi line stuff",
+    //         "This is awesome",
+    //     ]);
+    // println!("{table}");
+
+    let df = create_dataframe().unwrap();
+
+    table.set_header(df.get_column_names().iter().map(|s| s.to_string()).collect::<Vec<String>>());
+
+    let mut row = df.get_row(0).expect("unable to get first reference row");
+    for i in 0..df.height() {
+        for c in 0..df.width() {
+            let value = df.get_column(c).unwrap().get(i).unwrap();
+            row.0[c] = value;
+        }
+        df.get_row_amortized(i, &mut row).expect("row {i} doesn't match the first row");
+        let row_values: Vec<String> = row.0.iter().map(|v| v.to_string()).collect();
+        table.add_row(row_values);
+    }
+
+    for chunk in df.iter_chunks(CompatLevel::newest(), false) {
+        for row in chunk.iter() {
+            let row_values: Vec<String> = row.map(|v| v.to_string()).collect();
+            table.add_row(row_values);
+        }
+    }
 
     println!("{table}");
 }
